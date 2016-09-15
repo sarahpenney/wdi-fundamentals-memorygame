e18f71680e26b34af5012719dc5a2231e9d0a761
Sarah Penney <sarahpenneyuk@gmail.com>
2016-09-15 11:16:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+
+if (cardOne === cardTwo) {
+alert('You found a match! :)'); } 
+else { alert('Sorry, try again :(');
+
+var board = document.getElementById('game-board');
+function createBoard() {for (var i=0; i<cards.length; i++) 
+  {var cardElement = document.createElement('div');
+  cardElement.className = 'card';
